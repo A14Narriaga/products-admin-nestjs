c754c8884f4a1a1e4ecad29c4d8d32994dfa62ba
A14Narriaga <a14n.arriaga@gmail.com>
2024-07-14 17:00:18 -0600

    feat: production config

--- src/config/mongodb/mongodb.module.ts
+++ src/config/mongodb/mongodb.module.ts
@@ -2,16 +2,16 @@ import { Module } from "@nestjs/common"
 import { MongooseModule } from "@nestjs/mongoose"
 
 import { EnvConfig } from "../env"
-const { dbPort, dbUsername, dbPassword, dbHost, dbName } = EnvConfig()
+const { dbPort, dbUsername, dbPassword, dbHost, dbName, appEnv } = EnvConfig()
+const host = appEnv === "production" ? dbHost : `${dbHost}:${dbPort}`
+const baseUrl = appEnv === "production" ? "mongodb+srv" : "mongodb"
+const mongoUrl = `${baseUrl}://${dbUsername}:${dbPassword}@${host}`
 
 @Module({
 	imports: [
-		MongooseModule.forRoot(
-			`mongodb://${dbUsername}:${dbPassword}@${dbHost}:${dbPort}`,
-			{
-				dbName
-			}
-		)
+		MongooseModule.forRoot(mongoUrl, {
+			dbName
+		})
 	]
 })
 export class MongodbModule {}
